723e0e1456a61f8af96ec413f728a544ab59165e
Ronak Dhoot <ronak.dhoot@concirrus.com>
2017-09-21 20:15:55 +0530

    silly fix

--- app/Http/Controllers/DynamoDBController.php
+++ app/Http/Controllers/DynamoDBController.php
@@ -238,7 +238,7 @@ class DynamoDBController extends Controller
 
   function in_array_r($needle, $haystack, $strict = false) {
     foreach ($haystack as $item) {
-      if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+      if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && $this->in_array_r($needle, $item, $strict))) {
         return true;
       }
     }
