3c7c418240f51660880f7e2464c9688acdb49b28
Ronak Dhoot <ronak.dhoot@concirrus.com>
2018-10-27 23:04:24 +0530

    fixing minor bug :)

--- app/Http/Controllers/ElasticController.php
+++ app/Http/Controllers/ElasticController.php
@@ -157,8 +157,8 @@ class ElasticController {
 
         foreach ($this->scanFilterD as $k => $filter) {
           if($k < count($this->scanFilterD) - 1) {
-            if ($filter['id']['S'] != $item['data']['S']) {
-              unset($foundEmails[$filter['data']['S']]);
+            if ($filter['id']['S'] == $item['data']['S']) {
+              unset($foundEmails[$filter['id']['S']]);
               unset($this->scanFilterD[$k]);
             }
           }
